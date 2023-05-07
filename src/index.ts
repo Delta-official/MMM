56ef3a61313beb85c75510774f792f4d73429bd8
delta <darkussdelta@gmail.com>
2023-05-07 13:41:26 +0200

    allow escaping macro

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,10 @@ export async function start(): Promise<void> {
     if (messageMod) {
         inject.before(messageMod, "sendMessage", props => {
             let content: string = props[1].content;
-            let prefix = cfg.get("prefix") || macroPrefix 
+            let prefix = cfg.get("prefix") || macroPrefix;
             cfg.get("macros").forEach(({ macro, expansion }) => {
-                content = content.replaceAll(`${prefix}${macro}`, expansion)
+                let regex = RegExp(`(?<!\\\\)\\${prefix}${macro}`, "g")
+                content = content.replaceAll(regex, expansion)
             })
             props[1].content = content
             return props
